e1cf58f6d1d79ce37f0fe623268e61c6ed007c98
NixOS User <user@example.com>
2025-06-27 14:16:36 +0200

    refactor(privacy): Move all privacy imports to external config directory - Update all imports to use ~/.config/nixos-config/privacy/ - Remove local privacy files from repository - Update documentation and .gitignore

--- hosts/kobe/default.nix
+++ hosts/kobe/default.nix
@@ -3,7 +3,7 @@
     ./hardware.nix
     ./host-packages.nix
     ./host-services.nix
-    ./privacy-mask.nix
+    # privacy-mask.nix is imported in variables.nix, don't import it here
     ../../modules/core/openlinkhub-udev.nix
     ../../modules/core/openlinkhub.nix
   ];
